2499430b1b58ad3c68cab1711382bc0ab46c4150
michaelquesado <esquesado@gmail.com>
2023-06-15 11:15:28 -0300

    feat: converting to a list and cast to integer

--- foobar/lib/foobar.ex
+++ foobar/lib/foobar.ex
@@ -1,10 +1,14 @@
 defmodule Foobar do
   def build (file_name) do
-    file_name        |>
-      File.read()    |>
-      handler_file()
+    file_name
+      |> File.read()
+      |> handler_file()
   end
 
-  def handler_file ({:ok, result }) do result end
+  def handler_file ({:ok, result }) do
+    result
+      |> String.split(",")
+      |> Enum.map(&String.to_integer/1)
+  end
   def handler_file ({:error, reason }) do reason end
 end
