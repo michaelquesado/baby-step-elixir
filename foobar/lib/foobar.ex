d0921536c06f0cab91b6c4f5ea0a20d5a6e29d5b
michaelquesado <esquesado@gmail.com>
2023-06-15 16:05:57 -0300

    test

--- foobar/lib/foobar.ex
+++ foobar/lib/foobar.ex
@@ -4,27 +4,31 @@ import BarGuard, only: [is_bar: 1]
 defmodule Foobar do
   def build(file_name) do
     file_name
-      |> File.read()
-      |> handler_file()
+    |> File.read()
+    |> handler_file()
   end
 
-  def handler_file({:ok, content }) do
-    result = content
+  defp handler_file({:ok, content}) do
+    result =
+      content
       |> String.split(",")
       |> Enum.map(&convert_and_evaluate/1)
+
     {:ok, result}
   end
 
-  def handler_file({:error, reason }) do {:error, reason} end
+  defp handler_file({:error, reason}) do
+    {:error, reason}
+  end
 
-  def convert_and_evaluate (elem) do
+  def convert_and_evaluate(elem) do
     number = String.to_integer(elem)
     IO.inspect(number)
     evaluate(number)
   end
 
-  def evaluate(number) when is_foo(number) and is_bar(number), do: :foobar
-  def evaluate(number) when is_foo(number), do: :foo
-  def evaluate(number) when is_bar(number), do: :bar
-  def evaluate(number), do: number
+  defp evaluate(number) when is_foo(number) and is_bar(number), do: :foobar
+  defp evaluate(number) when is_foo(number), do: :foo
+  defp evaluate(number) when is_bar(number), do: :bar
+  defp evaluate(number), do: number
 end
--- foobar/test/foobar_test.exs
+++ foobar/test/foobar_test.exs
@@ -1,8 +1,14 @@
 defmodule FoobarTest do
   use ExUnit.Case
-  doctest Foobar
 
-  test "greets the world" do
-    assert Foobar.hello() == :world
+  describe "build/1" do
+    test "should return a converted list when a valid file is provided" do
+      assert Foobar.build("numbers.txt") ==
+               {:ok, [1, :foo, 4, 1, :bar, :foo, 7, :bar, :foo, 2, :foobar, 59, 1003]}
+    end
+
+    test "should return a error when a invalid list is provided" do
+      assert Foobar.build("invalid.txt") == {:error, :enoent}
+    end
   end
 end
