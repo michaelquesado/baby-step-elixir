4e4fca8e6d8a37fa116db01a933baf15ad62a4f6
michaelquesado <esquesado@gmail.com>
2023-06-19 11:57:51 -0300

    update game state after move

--- ex_mon/lib/games/actions/attack.ex
+++ ex_mon/lib/games/actions/attack.ex
@@ -1,4 +1,5 @@
 defmodule ExMon.Games.Actions.Attack do
+  alias ExMon.Game
   @move_rnd 10..35
   @move_avg 18..25
 
@@ -12,10 +13,18 @@ defmodule ExMon.Games.Actions.Attack do
   defp calculate_power(:soco), do: Enum.random(@move_avg)
 
   defp take_demage(demage, op) do
-   Map.get(op, :life)
+   life = Map.get(op, :life)
     |> make_demage(demage)
+
+   op = Map.put(op, :life, life)
+   player = String.to_atom(Map.get(op, :name))
+   Game.info()
+    |> Map.put(player, op)
+    |> Game.update()
+
   end
 
   defp make_demage(life, demage) when life - demage < 0, do: 0
   defp make_demage(life, demage), do: life - demage
+
 end
