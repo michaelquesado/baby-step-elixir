897e426e41d8b18c66f57d5bbbb7d6f48e9e480b
michaelquesado <esquesado@gmail.com>
2023-07-04 15:44:40 -0300

    using argon2 to encrypt user password

--- bank/lib/bank/users/user.ex
+++ bank/lib/bank/users/user.ex
@@ -2,10 +2,11 @@ defmodule Bank.Users.User do
   use Ecto.Schema
   import Ecto.Changeset
 
-  @fields [:name, :password_hash, :email, :cep]
+  @fields [:name, :password, :email, :cep]
 
   schema "users" do
     field :name, :string
+    field :password, :string, :virtual true
     field :password_hash, :string
     field :email, :string
     field :cep, :string
@@ -20,5 +21,12 @@ defmodule Bank.Users.User do
       |> validate_length(:name, min: 3)
       |> validate_format(:email, ~r/@/)
       |> validate_length(:cep, is: 8)
+      |> encrypt_password()
   end
+
+  defp encrypt_password(%Ecto.Changeset{valid?: true, changes: %{password: password}} = changeset) do
+    change(changeset, Argon2.add_hash(password))
+  end
+
+  defp encrypt_password(changeset), do: changeset
 end
